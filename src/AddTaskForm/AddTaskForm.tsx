45bb77cf64d3fee4e649189467b44ef6c102580e
Urmat Kushubekov <urmat.kushubekov@mail.ru>
2023-05-25 14:09:02 +0600

    Создал файл AddTaskForm в нем инпут и кнопка для добавление новых задач

---
+++ src/AddTaskForm/AddTaskForm.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface IAddTaskFormProps extends React.PropsWithChildren {
+    currentTasks: React.ChangeEventHandler<HTMLInputElement>;
+    onAddClick: React.MouseEventHandler;
+
+}
+const AddTaskForm: React.FC<IAddTaskFormProps> = props => {
+    return (
+        <div className="addText">
+            <input type="text" onChange={props.currentTasks}/>
+            <button className="btn" onClick={props.onAddClick}>Add</button>
+        </div>
+    );
+};
+
+export default AddTaskForm;
